7b7581987983182f8b9ff2d2c19670915a9afd83
Gauthier Billot <gauthier.billot@fishingcactus.com>
2020-04-11 22:04:47 +0200

    Looking good

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-build:
+build-website:
 	haxe build.hxml
--- src/Website.hx
+++ src/Website.hx
@@ -1,5 +1,14 @@
 class Website {
     static public function main() {
-        trace("in");
+        var t = new terminaljs.Terminal();
+        t.setHeight("100%");
+        t.setWidth("100%");
+        t.setBackgroundColor("rgba(0,0,0,0.2)");
+        t.setPrompt('<span style="color:red;">@</span>$ ');
+        js.Browser.document.body.appendChild(t.html);
+        t.print('Hello, world!');
+        t.input(function(input) {
+            trace(input);
+        });
     }
 }
